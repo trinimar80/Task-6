0ed45c9b5a59af1a42833f13dac06ef23fa6f94a
Marcela <marcela.valenciae@gmail.com>
2026-01-30 22:02:40 +0300

    commit inicial

--- vault_client.py
+++ vault_client.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def obtener_credenciales_db():
+def get_db_credentials():
    
-    # 1. Obtener configuración del entorno (inyectadas por Docker o .env)
+    """
+    Connects to Vault using AppRole and retrieves DB credentials.
+    Using 'hvac' 
+    """
     vault_url = os.getenv('VAULT_ADDR', 'http://vault:8200')
     role_id = os.getenv('VAULT_ROLE_ID')
     secret_id = os.getenv('VAULT_SECRET_ID')
 
-    # Validación básica de configuración
+   
     if not role_id or not secret_id:
         print("ERROR: VAULT_ROLE_ID o VAULT_SECRET_ID no están configurados.")
         return None, None
@@ -23,19 +26,18 @@ def obtener_credenciales_db():
         # 3. Autenticarse con AppRole (Método recomendado para aplicaciones)
         client.auth.approle.login(role_id=role_id, secret_id=secret_id)
 
-        # 4. Leer el secreto (Motor KV v2)
-        # Por defecto buscamos en el mount 'secret' y el path definido
+        # 4. Read secret (Motor KV v2)
         read_response = client.secrets.kv.v2.read_secret_version(
             mount_point='secret',
             path='library_db'
         )
 
         # Extraer los datos del diccionario de respuesta
-        datos = read_response['data']['data']
+        credentials = read_response['data']['data']
         
         # Devolvemos usuario y contraseña
-        return datos['username'], datos['password']
+        return credentials['username'], credentials['password']
 
     except Exception as e:
-        print(f"❌ Error crítico al conectar con Vault: {e}")
+        print(f"❌ Error connecting to Vault: {e}")
         return None, None
